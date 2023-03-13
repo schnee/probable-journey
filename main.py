300cb2a70cc7674c384d5085479e4999e64b3f1d
Brent <schneeman@gmail.com>
2023-03-13 11:02:06 -0500

    randomized directions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import math
+import random
 import time
 
 
@@ -10,9 +12,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     start = time.time()
-    time.sleep(10)
+    time.sleep(.1)
     elapsed = time.time() - start
     print(f'Elapse: {elapsed}')
+    meters_per_step_base = 3
+    step_range = meters_per_step_base * 0.1
+    print(random.uniform(meters_per_step_base - step_range, meters_per_step_base + step_range))
+    uniform = random.uniform(0, 360)
+    print(uniform, math.radians(uniform))
 
 
 # Press the green button in the gutter to run the script.
