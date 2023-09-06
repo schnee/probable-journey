4ecfefbf2d74fa0bf796d852ca3fe57bc0dc1bb6
Brent <schneeman@gmail.com>
2023-09-06 11:12:28 -0500

    externalized the stop type to filter upon

--- optimal_journey.py
+++ optimal_journey.py
@@ -1,6 +1,23 @@
+import argparse
 import requests
 from haversine import haversine, Unit
 import pandas as pd    
+
+###
+#var males = [4, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39];
+#var females = [5, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38];
+
+#var gruntDict = {4:"grunt",5:"grunt",6:"bug",7:"bug",48:"ghost",47:"ghost",10:"dark",
+# 11:"dark",12:"dragon",13:"dragon",14:"fairy",15:"fairy",16:"fighting",17:"fighting",
+# 18:"fire",19:"fire",20:"flying",21:"flying",22:"grass",23:"grass",24:"ground",
+# 25:"ground",26:"ice",27:"ice",28:"metal",29:"metal",30:"normal",31:"normal",
+# 32:"poison",33:"poison",34:"psychic",35:"psychic",36:"rock",37:"rock",38:"water",
+# 39:"water",49:"electric",50:"electric",41:"cliff",42:"arlo",43:"sierra",
+# 44:"giovanni",500: "npc",501: "npc",502: "npc",503: "npc",504: "npc",505: "npc",
+# 506: "npc",507: "npc",508: "npc",509: "npc",510: "npc", 9999:"kecleon", 
+# 9998:"gimmighoul_coin", 9997:"showcase"};
+###
+
 
 
 def get_invasions():
@@ -27,6 +44,15 @@ def cooldown(dist, df_cool):
     return df.iat[0,1]
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description = 'Optimal Journey')
+    parser.add_argument('-t', '--type', help='the numeric type of the pokestop')
+    args = parser.parse_args()
+
+    stopType = int(args.type.strip())
+    
+    print(f'stop type {stopType}')
+
     df_lastloc = get_lastloc()
     last_lat = df_lastloc.at[0,'lat']
     last_lng = df_lastloc.at[0,'lng']
@@ -43,7 +69,7 @@ if __name__ == '__main__':
 
 
     # filter to just the invasions of interest
-    df2 = df[df['character'] == 12].reset_index(drop=True)
+    df2 = df[df['character'] == stopType].reset_index(drop=True)
     #print(df2)
 
     # now the main logic. first, calculate the distance to each 
@@ -68,7 +94,7 @@ if __name__ == '__main__':
     df2 = df2[(rtime+df2['cool']) < df2['invasion_end']].sort_values(by='distance')
 
     df2.apply(lambda row: 
-              print(f"{row['lat']},{row['lng']} {row['cool']} {row['name']}"), 
+              print(f" {row['lat']},{row['lng']} {row['cool']} {row['name']}"), 
               axis =1 )
 
     
