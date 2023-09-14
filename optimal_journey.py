d1e76a68e1d78263ab50c4601ad86c64e4655516
Brent <schneeman@gmail.com>
2023-09-14 09:30:04 -0500

    modified

--- optimal_journey.py
+++ optimal_journey.py
@@ -1,7 +1,8 @@
 import argparse
 import requests
 from haversine import haversine, Unit
-import pandas as pd    
+import pandas as pd 
+from textual.app import App
 
 ###
 #var males = [4, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39];
@@ -35,7 +36,6 @@ def get_cooldown():
 
 def cooldown(dist, df_cool):
      
-
     #subset df_cool to just the values >= dist
     df = df_cool[df_cool['distance'] >= dist]
 
@@ -43,16 +43,8 @@ def cooldown(dist, df_cool):
 
     return df.iat[0,1]
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description = 'Optimal Journey')
-    parser.add_argument('-t', '--type', help='the numeric type of the pokestop')
-    args = parser.parse_args()
-
-    stopType = int(args.type.strip())
+def get_stops(stopType):
     
-    print(f'stop type {stopType}')
-
     df_lastloc = get_lastloc()
     last_lat = df_lastloc.at[0,'lat']
     last_lng = df_lastloc.at[0,'lng']
@@ -92,6 +84,19 @@ if __name__ == '__main__':
     # before they expire. current_time + cool < expiration...
 
     df2 = df2[(rtime+df2['cool']) < df2['invasion_end']].sort_values(by='distance')
+    return df2
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description = 'Optimal Journey')
+    parser.add_argument('-t', '--type', help='the numeric type of the pokestop')
+    args = parser.parse_args()
+
+    stopType = int(args.type.strip())
+       
+    print(f'stop type {stopType}')
+
+    df2 = get_stops(stopType)
 
     df2.apply(lambda row: 
               print(f" {row['lat']},{row['lng']} {row['cool']} {row['name']}"), 
